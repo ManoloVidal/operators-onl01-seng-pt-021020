68ee209d2f1a23d38cf9509f43c691e95d8910bf
Juan Vidal-Ramos <vidalramosjm@gmail.com>
2020-02-13 22:11:04 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -18,11 +18,6 @@ def not_safe?(speed)
   #(conditional) ? 'true/truthy' : 'false/falsey'
   if speed>60
     (speed>=40 && speed<=60) ? false : true
-    true
-    elsif speed<40
-    true
-    elsif speed>=40 && speed<=60
-    false 
   end
 	
 end
